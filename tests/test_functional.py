9956977eba772af4d086ed742a1e3cca4fa72d9b
Bibiana Cristofol Amat <bcristofol@gmail.com>
2018-08-17 09:53:21 +0100

    Start setup selenium

---
+++ tests/test_functional.py
@@ -0,0 +1,45 @@
+import os
+
+import pytest
+from flask import url_for
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+os.environ['FLASK_ENV'] = 'development'
+BROWSER = os.environ.get('BROWSER', 'ChromeHeadless')
+
+
+@pytest.fixture(scope="module")
+def browser(request):
+    if BROWSER == 'ChromeHeadless':
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+    else:
+        browser = getattr(webdriver, BROWSER)()
+    browser.implicitly_wait(3)
+    request.addfinalizer(lambda: browser.quit())
+    return browser
+
+
+@pytest.mark.usefixtures('live_server')
+def hover_over_the_menu(browser):
+    browser.get(url_for('data_registry', _external=True))
+    element = browser.find_element_by_id('menu')
+    hover = ActionChains(browser).move_to_element(element)
+    hover.perform()
+
+
+@pytest.mark.usefixtures('live_server')
+def test_nav_menu(browser):
+    browser.get(url_for('data_registry', _external=True))
+
+    text_body = browser.find_element_by_tag_name('body').text
+
+    assert 'Home' in text_body
+    assert 'Core' not in text_body
+    assert 'Community' not in text_body
+    assert 'Selector' not in text_body
