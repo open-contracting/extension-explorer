d62dfd9efec014036d1347f0b17a1051b19df4e9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-01-10 13:26:52 -0500

    Add 404 tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -29,3 +29,33 @@ def test_extension_schema(client):
 def test_extension_codelists(client):
     response = client.get(url_for('extension_codelists', lang='en', slug='location', version='v1.1.3'))
     assert response.status_code == 200
+
+
+def test_extension_404_extension(client):
+    response = client.get(url_for('extension', lang='en', slug='nonexistent', version='v1.1.3'))
+    assert response.status_code == 404
+
+
+def test_extension_schema_404_extension(client):
+    response = client.get(url_for('extension_schema', lang='en', slug='nonexistent', version='v1.1.3'))
+    assert response.status_code == 404
+
+
+def test_extension_codelists_404_extension(client):
+    response = client.get(url_for('extension_codelists', lang='en', slug='nonexistent', version='v1.1.3'))
+    assert response.status_code == 404
+
+
+def test_extension_404_version(client):
+    response = client.get(url_for('extension', lang='en', slug='location', version='nonexistent'))
+    assert response.status_code == 404
+
+
+def test_extension_schema_404_version(client):
+    response = client.get(url_for('extension_schema', lang='en', slug='location', version='nonexistent'))
+    assert response.status_code == 404
+
+
+def test_extension_codelists_404_version(client):
+    response = client.get(url_for('extension_codelists', lang='en', slug='location', version='nonexistent'))
+    assert response.status_code == 404
