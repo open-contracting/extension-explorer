2f98f9ae95df27609baf9a1f330df10ea309115c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-01-17 12:50:53 -0500

    Update tests for util

--- tests/test_util.py
+++ tests/test_util.py
@@ -49,9 +49,10 @@ release_schema = {
                 None
             ]
         },
-        "typeRef": {
-            "title": "Type",
-            "$ref": "#/definitions/Asset"
+        "external": {
+            "title": "External",
+            "type": "string",
+            "$ref": "#/definitions/Value"
         },
         "field": {
             "title": "Field",
@@ -104,12 +105,62 @@ def test_get_schema_tables():
             ['ref', 'Asset', '', '<a href="#asset">Asset</a> object'],
             ['refArray', 'Assets', '', 'array of <a href="#asset">Asset</a> objects'],
             ['null', 'Null', '', ''],
-            ['typeRef', 'Type', '', '<a href="#asset">Asset</a> object'],
+            ['external', 'External', '', '<a href="http://standard.open-contracting.org/1.1/en/schema/reference/#value">Value</a> object'],  # noqa
             ['field', 'Field', '', 'object'],
             ['field/subfield', '', 'Subfield', 'object'],
             ['field/subfield/subsubfield', '', 'Subsubfield', ''],
         ],
     }
+
+
+def test_get_schema_tables_mixed_array_success():
+    extension_version = deepcopy(extension_version_template)
+    extension_version['schemas']['release-schema.json']['en'] = {
+        "properties": {
+            "nullArray": {
+                "title": "Array",
+                "type": [
+                    "array",
+                    "null"
+                ],
+                "items": {
+                    "type": "string"
+                }
+            }
+        }
+    }
+
+    tables = get_schema_tables(extension_version, 'en')
+
+    assert dict(tables) == {
+        'Release': [
+            ['nullArray', 'Array', '', 'array of strings'],
+        ]
+    }
+
+
+def test_get_schema_tables_mixed_array_failure():
+    extension_version = deepcopy(extension_version_template)
+    extension_version['schemas']['release-schema.json']['en'] = {
+        "properties": {
+            "mixedArray": {
+                "title": "Array",
+                "type": [
+                    "array",
+                    "string"
+                ],
+                "items": {
+                    "title": "String",
+                    "type": "string"
+                }
+            }
+        }
+    }
+
+    with pytest.raises(NotImplementedError) as excinfo:
+        get_schema_tables(extension_version, 'en')
+
+    assert str(excinfo.value) == 'array / string is not implemented'
 
 
 def test_get_schema_tables_object_array():
@@ -159,7 +210,7 @@ def test_get_schema_tables_array_array():
 
 
 def test_identify_headings():
-    html = commonmark.commonmark('# A heading\nText\n###### A heading\nText\n## A heading\nText\n### Another heading\nText\n#### A heading\n')  # noqa
+    html = commonmark.commonmark('# A heading\nText\n###### A heading\nText\n## A heading\nText\n### Another heading\nText\n#### Changelog\nText\n##### v1.0.0\nText\n#### A heading\nText\n')  # noqa
 
     html, headings = identify_headings(html)
 
@@ -168,6 +219,7 @@ def test_identify_headings():
         {'id': 'a-heading-1', 'level': 2, 'text': 'A heading'},
         {'id': 'a-heading-2', 'level': 2, 'text': 'A heading'},
         {'id': 'another-heading', 'level': 3, 'text': 'Another heading'},
+        {'id': 'changelog', 'level': 4, 'text': 'Changelog'},
         {'id': 'a-heading-3', 'level': 4, 'text': 'A heading'},
     ]
 
@@ -179,7 +231,12 @@ def test_identify_headings():
 <p>Text</p>
 <h3 id="another-heading">Another heading</h3>
 <p>Text</p>
+<h4 id="changelog">Changelog</h4>
+<p>Text</p>
+<h5>v1.0.0</h5>
+<p>Text</p>
 <h4 id="a-heading-3">A heading</h4>
+<p>Text</p>
 </div>"""
 
 
