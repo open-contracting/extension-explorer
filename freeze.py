a2da8a6090ddcd8b196d3c7ce96569619f35caa9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-01-23 11:44:53 -0500

    Add URL generator to freeze redirection pages

--- freeze.py
+++ freeze.py
@@ -1,9 +1,17 @@
 from flask_frozen import Freezer
 
-from extension_explorer.views import app
-
+from extension_explorer.util import get_extensions
+from extension_explorer.views import app, LANGS
 
 freezer = Freezer(app)
+
+
+@freezer.register_generator
+def extension():
+    for lang in LANGS:
+        for identifier in get_extensions():
+            yield {'lang': lang, 'identifier': identifier}
+
 
 if __name__ == '__main__':
     freezer.freeze()
