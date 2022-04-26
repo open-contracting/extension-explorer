583d7aae995a96c483e94d309f65e47d6cdb0e69
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-26 17:31:28 -0400

    build: Add license metadata to avoid false positive for licensing compliance

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ocdsextensionexplorer',
     version='0.0.0',
+    license='BSD',
     packages=find_packages(),
     entry_points={
         'babel.extractors': [
