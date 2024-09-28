08bbd6040fc78d7f53417d251bda8f00abe73b64
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-28 16:31:40 -0400

    chore: Avoid unnecessary assignment before for-loop

--- extension_explorer/extract.py
+++ extension_explorer/extract.py
@@ -3,7 +3,6 @@ from yaml import safe_load
 
 def extract_tag(fileobj, keywords, comment_tags, options):
     """Yield the title values in the YAML file."""
-    data = safe_load(fileobj)
-    for prefix, tags in data.items():
+    for prefix, tags in safe_load(fileobj).items():
         for i, tag in enumerate(tags):
             yield 1, '', tag['title'], [f'/{prefix}/{i}/title']
