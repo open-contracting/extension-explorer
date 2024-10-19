dcca464f8c88fa4ac8bc52726962b6a5cce5d845
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-10-18 20:59:37 -0400

    chore: Never use relative import

--- extension_explorer/views.py
+++ extension_explorer/views.py
@@ -8,7 +8,7 @@ from flask_babel import Babel, gettext
 from flask_env import MetaFlaskEnv
 from werkzeug.exceptions import NotFound
 
-from .util import (
+from extension_explorer.util import (
     get_codelist_tables,
     get_extension_explorer_data_filename,
     get_extensions,
