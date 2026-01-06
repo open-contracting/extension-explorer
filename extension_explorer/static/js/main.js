4eb5e3e7e6c7b6d03994edfe8e52eea5a34aafde
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-01-06 00:19:51 -0500

    security: Remove "unsafe-inline" scripts. Update tooltip title code to match Bootstrap 5.

---
+++ extension_explorer/static/js/main.js
@@ -0,0 +1,40 @@
+try{Typekit.load({ async: true });}catch(e){}
+
+$(function () {
+  // extensions.html
+  if ($('.filter-select').length) {
+    var filters = {};
+
+    $('.filter-select').on('change', function () {
+      var prop, selector = '', selected;
+
+      filters[this.id] = this.value;
+      for (prop in filters) {
+        selector += filters[prop];
+      }
+
+      selected = $('.extension' + selector).show();
+      if (selector) {
+        $('.extension:not(' + selector + ')').hide();
+      }
+
+      $('#filter-count').text(selected.length);
+    });
+  }
+
+  // layout_extension.html
+  if (typeof ClipboardJS !== 'undefined' && $('.clipboard').length) {
+    var clipboard = new ClipboardJS('.clipboard');
+
+    clipboard.on('success', function (e) {
+      var $trigger = $(e.trigger);
+      var original = $trigger.data('bs-original-title');
+      var updated = $trigger.data('updated-title');
+
+      $trigger.attr('data-bs-original-title', updated).tooltip('show').attr('data-bs-original-title', original);
+      e.clearSelection();
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+});
