70c9c15f8979e3a9c08d8e200d68ece59e586efd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-01-06 00:23:16 -0500

    chore: Run biome format .

--- extension_explorer/static/js/main.js
+++ extension_explorer/static/js/main.js
@@ -1,37 +1,41 @@
-try{Typekit.load({ async: true });}catch(e){}
+try {
+  Typekit.load({ async: true });
+} catch (e) {}
 
 $(function () {
   // extensions.html
-  if ($('.filter-select').length) {
+  if ($(".filter-select").length) {
     var filters = {};
 
-    $('.filter-select').on('change', function () {
-      var prop, selector = '', selected;
+    $(".filter-select").on("change", function () {
+      var prop,
+        selector = "",
+        selected;
 
       filters[this.id] = this.value;
       for (prop in filters) {
         selector += filters[prop];
       }
 
-      selected = $('.extension' + selector).show();
+      selected = $(".extension" + selector).show();
       if (selector) {
-        $('.extension:not(' + selector + ')').hide();
+        $(".extension:not(" + selector + ")").hide();
       }
 
-      $('#filter-count').text(selected.length);
+      $("#filter-count").text(selected.length);
     });
   }
 
   // layout_extension.html
-  if (typeof ClipboardJS !== 'undefined' && $('.clipboard').length) {
-    var clipboard = new ClipboardJS('.clipboard');
+  if (typeof ClipboardJS !== "undefined" && $(".clipboard").length) {
+    var clipboard = new ClipboardJS(".clipboard");
 
-    clipboard.on('success', function (e) {
+    clipboard.on("success", function (e) {
       var $trigger = $(e.trigger);
-      var original = $trigger.data('bs-original-title');
-      var updated = $trigger.data('updated-title');
+      var original = $trigger.data("bs-original-title");
+      var updated = $trigger.data("updated-title");
 
-      $trigger.attr('data-bs-original-title', updated).tooltip('show').attr('data-bs-original-title', original);
+      $trigger.attr("data-bs-original-title", updated).tooltip("show").attr("data-bs-original-title", original);
       e.clearSelection();
     });
 
