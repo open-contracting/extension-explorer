6d37b7074653f905158bbca8da0e7c307ca635af
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-08-12 22:35:42 -0400

    a11y: Remove optional key

--- pa11y.default.js
+++ pa11y.default.js
@@ -85,7 +85,7 @@ function createDefaults(extraKnownWarnings = []) {
 }
 
 module.exports = {
-  createDefaults: createDefaults,
+  createDefaults,
   defaults: createDefaults(),
   urls: [
     "http://127.0.0.1:8000/en/",
